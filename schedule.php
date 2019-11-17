e866532e45ab337ebd2c811c6f636c57c2573a87
sandra <sandramwende777@gmail.com>
2019-11-17 14:49:23 +0300

    gfd

--- schedule.php
+++ schedule.php
@@ -6,9 +6,68 @@
 	td{
 		color: white;
 	}
+
+	
+body {
+  font-family: "Lato", sans-serif;
+}
+
+.sidenav {
+  height: 100%;
+  width: 0;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  background-color: #111;
+  overflow-x: hidden;
+  transition: 0.5s;
+  padding-top: 60px;
+}
+
+.sidenav a {
+  padding: 8px 8px 8px 32px;
+  text-decoration: none;
+  font-size: 25px;
+  color: #818181;
+  display: block;
+  transition: 0.3s;
+}
+
+.sidenav a:hover {
+  color: #f1f1f1;
+}
+
+.sidenav .closebtn {
+  position: absolute;
+  top: 0;
+  right: 25px;
+  font-size: 36px;
+  margin-left: 50px;
+}
+
+#main {
+  transition: margin-left .5s;
+  padding: 16px;
+}
+
+@media screen and (max-height: 450px) {
+  .sidenav {padding-top: 15px;}
+  .sidenav a {font-size: 18px;}
+}
+
 </style>
 </head>
 <body bgcolor="black">
+
+	<div id="mySidenav" class="sidenav">
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+  <a href="home.php">Home</a>
+  <a href="schedule.php">Schedule</a>
+  <a href="units.php">Units</a>
+  <a href="students.php">Students</a>
+</div>
+
 <H1><FONT COLOR="white"><CENTER>SCHEDULE</FONT></H1>
 <th scope="col">#</th>
  <table class="table">
@@ -63,8 +122,8 @@
 
 </tr>
 
-<div id="open">
-  <span style="font-size:30px;cursor:pointer;color: white" onclick="openNav()">&#9776; menu href</span>
+</div >
+  <span style="font-size:30px;cursor:pointer;color: white" onclick="openNav()">&#9776; menu</span>
 
 </div>
 <script>
--- students.php
+++ students.php
@@ -1,3 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="assets/stylesheets/pages.min.css">
+ <link href="assets/stylesheets/landerapp.min.css" rel="stylesheet" type="text/css">
+        <link href="assets/stylesheets/themes.min.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="bootstrap/css/nazstyle.css">
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+
 <?php
 
 $connection = mysqli_connect("localhost","root","","csc316");
@@ -8,12 +28,12 @@ if(isset($_GET['id'])){
 else{
 	$query = "SELECT * FROM st as a,units as b where b.UNIT_ID = a.ST_UNIT";
 }
-echo "<table><tr><th>Student Id</th><th>Name</th><th>Unit</th><th>Status</th></tr>";
+echo "<table class='table'><thead class='thead-dark'><tr><th scope='col'>Student Id</th><th scope='col'>Name</th><th scope='col'>Unit</th><th scope='col'>Status</th></thead></tr>";
 $result = mysqli_query($connection,$query);	
 if (mysqli_num_rows($result) > 0) {
     while($row = mysqli_fetch_assoc($result)) {
     	echo "<tr>";
-    	echo "<td>".$row["ST_ID"]."</td>";
+    	echo "<td scope='row'>".$row["ST_ID"]."</td>";
     	echo "<td>".$row["ST_NAME"]."</td>";
     	echo "<td>".$row["UNIT_NAME"]."</td>";
     	echo "<td>".$row["ST_STATUS"]."</td>";
@@ -26,17 +46,7 @@ if (mysqli_num_rows($result) > 0) {
     echo "0 results";
 }
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-	<meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-	 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-</head>
-<body>
+
 
 </body>
 </html>
